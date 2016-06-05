f15f4d94fb109f9f7b453a947380a5cbc8b23e14
unknown <>
2016-06-06 07:28:36 +0900

    Angular2のバインディングのテスト

---
+++ Angular2BindTest/Angular2BindTest/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+    title = 'Angular2テストだよ';
+    nameSatou = '佐藤';
+    nameTanaka = '田中';
+    nameSuzuki = '鈴木';
+}
